20d72d275d97792d00ac408d55d9379922f746ac
schwarka <schwartz.ka@gmail.com>
2015-07-01 22:43:20 -0400

    First attempt at rust docker, failing on Windows with boot2docker

---
+++ rust/install.sh
@@ -0,0 +1,17 @@
+#! /bin/bash
+
+# exit if a command fails
+set -e
+
+# install curl (needed to install rust)
+apt-get update && apt-get install -y curl
+
+# install rust and cargo (stable)
+curl -sSf https://static.rust-lang.org/rustup.sh | sh
+
+# cleanup package manager
+apt-get remove --purge -y curl && apt-get autoclean && apt-get clean
+rm -rf /var/lib/apt/lists/* /tmp/* /var/tmp/*
+
+# prepare dir to share with host
+mkdir /source
